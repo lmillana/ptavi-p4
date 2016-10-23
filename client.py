d4295c7ec687378b7ac323a6e29d5d1653707df5
lmillana <leyreeemcaballero@gmail.com>
2016-10-23 19:53:42 +0200

    Ejercicio 4 P4 Corregido

--- client.py
+++ client.py
@@ -20,8 +20,8 @@ Line = ' '.join(sys.argv[3:])
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((Ip, Port))
     print("Enviando:", Line)
-    my_socket.send(bytes(Line, 'utf-8') + b'\r\n') 
-    data = my_socket.recv(1024) 
+    my_socket.send(bytes(Line, 'utf-8') + b'\r\n')
+    data = my_socket.recv(1024)
     print('Recibido -- ', data.decode('utf-8'))
 
 print("Socket terminado.")
