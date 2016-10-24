c06299a3d1de685a9f7e1d3fd1f098efe7bdd020
lmillana <leyreeemcaballero@gmail.com>
2016-10-24 15:15:42 +0200

    Ejercicio 6 P4 Cliente modificado

--- client.py
+++ client.py
@@ -7,21 +7,21 @@ Programa cliente UDP que abre un socket a un servidor
 import socket
 import sys
 
-    # Constantes(Mayusculas). Dirección IP del servidor y contenido a enviar
-    #SERVER = 'localhost'
-    #PORT = 6001
-    #LINE = '¡Hola mundo!'
 
 Ip = str(sys.argv[1])
 Port = int(sys.argv[2])
-Line = ' '.join(sys.argv[3:])
+Method = sys.argv[3].upper()
+Line = ' '.join(sys.argv[4:])
+
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.connect((Ip, Port))
-    print("Enviando:", Line)
-    my_socket.send(bytes(Line, 'utf-8') + b'\r\n')
+    #Creamos la petición SIP:
+    print('Enviando:', Method, 'sip:', Line, 'SIP/2.0\r\n\r\n')
+    #Enviamos la petición:
+    my_socket.send(bytes(Method, 'utf-8') + b' sip:'+ bytes(Line,'utf-8') + b' SIP/2.0\r\n\r\n')
     data = my_socket.recv(1024)
-    print('Recibido -- ', data.decode('utf-8'))
+    print('Recibido? -- ', data.decode('utf-8'))
 
 print("Socket terminado.")
